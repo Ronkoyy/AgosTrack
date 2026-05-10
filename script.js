72956703ad7254c3e1509c9ca603cf56bb17f059
Atasha Alecer <atashaalecer@gmail.com>
2026-05-10 11:02:24 +0800

    checks if the user has existing account, and prompt them then to og in if they try to access the dashboard

--- script.js
+++ script.js
@@ -7,3 +7,57 @@ let currentUserName = sessionStorage.getItem("loggedInName") || "Ranger";
 const currentPage = window.location.pathname.split("/").pop();
 const protectedPages = ["dashboard.html", "map.html", "report.html", "profile.html"];
 
+const app = {
+    map: null, 
+    markers: [], 
+    tempMarker: null,
+    wasteChartInstance: null,
+    statusChartInstance: null,
+    severityChartInstance: null, 
+    dashboardData: null, 
+    reportToDelete: null, 
+    allReports: [],
+
+    async init() {
+        console.log("AgosTrack Supabase Engine Initializing...");
+        
+        //check if the user is logged in and redirect to login page if not.
+        const { data: { session } } = await supabaseClient.auth.getSession();
+        if (session && session.user) {
+            currentUserEmail = session.user.email;
+            sessionStorage.setItem("loggedInEmail", currentUserEmail);
+        } else if (protectedPages.includes(currentPage) || (currentPage === '' && protectedPages.includes('dashboard.html'))) {
+            
+            window.location.href = "login.html";
+            return;
+        }
+
+        this.fixLeafletIcons();
+        this.updateGlobalUI();
+         if (currentPage === 'dashboard.html' || currentPage === '') this.loadDash();
+        if (currentPage === 'map.html') this.loadMap();
+        if (currentPage === 'report.html') this.loadPickerMap();
+        if (currentPage === 'profile.html') this.loadProfile();
+
+        
+        // Attaches events to our forms so they don't refresh the page when submitted
+        const loginForm = document.getElementById('login-form');
+        if (loginForm) loginForm.addEventListener('submit', (e) => { e.preventDefault(); this.login(e); });
+
+        const signupForm = document.getElementById('signup-form');
+        if (signupForm) signupForm.addEventListener('submit', (e) => { e.preventDefault(); this.signup(e); });
+
+        const reportForm = document.getElementById('report-form');
+        if (reportForm) reportForm.addEventListener('submit', (e) => { e.preventDefault(); this.handleReport(e); });
+
+        const editProfileForm = document.getElementById('edit-profile-form');
+        if (editProfileForm) editProfileForm.addEventListener('submit', (e) => { e.preventDefault(); this.saveProfile(e); });
+
+
+
+
+
+
+
+
+};
