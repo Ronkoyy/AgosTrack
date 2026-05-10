1068d40ddc033aa65db3220523d7d54059c8ece6
Eron Moquete <eronmoquete4@gmail.com>
2026-05-10 10:25:05 +0800

    Establsihed connectio to supabase

---
+++ script.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://nlduwtqzuaogqzvabdey.supabase.co'; 
+const SUPABASE_ANON_KEY = 'sb_publishable_Zm4qZfegIRn0BhEUbxXk2Q_Jy2t8S3q'; 
+
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
