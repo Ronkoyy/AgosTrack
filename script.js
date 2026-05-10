68548f4356b5cb63222bc94e43063297ab9a190e
Eron Moquete <eronmoquete4@gmail.com>
2026-05-10 10:28:24 +0800

    Implemented session storage so that the user stays logged in even after refreshing the pag.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const SUPABASE_URL = 'https://nlduwtqzuaogqzvabdey.supabase.co'; 
 const SUPABASE_ANON_KEY = 'sb_publishable_Zm4qZfegIRn0BhEUbxXk2Q_Jy2t8S3q'; 
-
-
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+//to let the user stay lgged in even after refreshing the page.
+let currentUserEmail = sessionStorage.getItem("loggedInEmail") || null;
+let currentUserName = sessionStorage.getItem("loggedInName") || "Ranger"; 
+const currentPage = window.location.pathname.split("/").pop();
+const protectedPages = ["dashboard.html", "map.html", "report.html", "profile.html"];
+
